a113e4470466f1068a1905caf0df352c1e2b0234
Christina Burchette <burchc426@gmail.com>
2018-12-20 01:25:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function calculatesFarePrice (start, destination) {
   let actualDistance = (destination - start) * 264
   let distanceTraveled = (((Math.abs(destination - start)) * 264) - 400)
   let farePrice = (((destination - start) * 264) - 400)* 0.02
-  if (actualDistance >= 2500) {return "cannot travel that far"}
+  if (actualDistance >=2500) {return "cannot travel that far"}
   if (actualDistance >=2000) {return 25}
   if (actualDistance <=400) {return 0}
   if (distanceTraveled<=2000) {return farePrice}
